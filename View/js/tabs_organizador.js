cadce567b850f08c532ea0f6872278ca2223f98d
Sergio-a11 <cz.andrs@gmail.com>
2022-12-13 14:15:23 -0500

    tabla adm con botones sin info especifica

--- View/js/tabs_organizador.js
+++ View/js/tabs_organizador.js
@@ -18,21 +18,21 @@ function pillActive(activo) {
     </li>
     `;
     pestanaActiva = activo;
-    cargarTabla(activo);
+    cargarTabla(tabsId[activo]);
 }
 
 
 
 async function cargarTabla(activo) {
 
-  const elegirTabla = {"tabPartidos": "Partidos",
-  "tabParticipantes":"Participantes",
-  "tabSalas":"Salas",
-  "tabCiudad":"Ciudades",
-  "tabHistorico":"Campeonatos"
+  const elegirTabla = {"tabPartidos": "Partido/getPartidos",
+  "tabParticipantes":"Participante/getParticipantes",
+  "tabSalas":"Sala/getSalas",
+  "tabCiudad":"Ciudad/getCiudades",
+  "tabHistorico":"Campeonato/getCampeonatos"
 };
 
-  const request = await fetch('http://localhost/tenisdemesa/Partido/getPartidos', {
+  const request = await fetch(`http://localhost/tenisdemesa/${elegirTabla[activo]}`, {
     method: 'GET',
         headers: {
             'Accept': 'application/json',
@@ -49,8 +49,9 @@ async function cargarTabla(activo) {
     encabezados.forEach(i => {
       columnsTable += `<th scope="col">${i}</th>`;
     });
+    columnsTable += `<th scope="col">Editar</th><th scope="col">Eliminar</th>`;
     data.forEach(i => {
-      console.log("i",i);
+      //console.log("i",i);
       let contHead = 0;
       let valores = "";
       
@@ -58,6 +59,17 @@ async function cargarTabla(activo) {
         //console.log("j",j)
         valores += `<td>${i[encabezados[contHead++]]}</td>`;
       })
+      valores += `<td><button class="btn btn-danger" type="button">
+      <a href="index.html" style="text-decoration: none; color:white"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+      </svg></a>
+      </button></td><td><button class="btn btn-danger" type="button">
+      <a href="index.html" style="text-decoration: none; color:white"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+      <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+      </svg></a>
+      </button></td>`
       dataTable += `
       <tr>${valores}</tr>
         `;
@@ -79,58 +91,6 @@ async function cargarTabla(activo) {
   }
   else
     console.log("🚀", "No");
-  /*
-    let url = "http://localhost/tenisdemesa/Partido/getPartidos";
-    let http = new XMLHttpRequest;
-    http.open("GET",url,true);
-    http.send();
-    http.onreadystatechange = () => {
-      if(this.readyState == 4 && this.status == 200)
-      {
-        let res = JSON.parse(responseText);
-        console.log("🚀 ~ file: tabs_organizador.js:33 ~ cargarTabla ~ res", res)
-      }
-    };
-    
-
-    let columns = [`
-        <th scope="col">#</th>
-        <th scope="col">Fecha</th>
-        <th scope="col">Juez</th>
-        <th scope="col">Jugador 1 </th>
-        <th scope="col">Jugador 2</th>
-        <th scope="col">Marcador</th>
-        <th scope="col">Comentarios</th>
-        <th scope="col">Sala</th>
-        <th scope="col">Entradas Vendidas</th>`, 
-        `<th scope="col"># Asociado</th>
-        <th scope="col">Nombre</th>
-        <th scope="col">Direccion</th>
-        <th scope="col">Nivel de Juego</th>
-        <th scope="col">Correo</th>
-        <th scope="col">Ciudad</th>
-        `];
-    let tabla = `
-    <table class="table" id="tbOrganizador">
-          <thead>
-            <tr>
-              ${columns[activo]}
-            </tr>
-          </thead>
-          <tbody class="table-group-divider">
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark&nbsp;&nbsp;<img src="http://localhost/tenisdemesa/View/resources/trophy-fill.svg" alt="Ganador"></td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>Jacob&nbsp;&nbsp;<img src="http://localhost/tenisdemesa/View/resources/trophy-fill.svg" alt="Ganador"></td>
-              <td>Thornton</td>
-              <td>@fat</td>
-              <td>Jacob&nbsp;&nbsp;<img src="http://localhost/tenisdemesa/View/resources/trophy-fill.svg" alt="Ganador"></td>
-              <td>Thornton</td>
-            </tr>
-           </tbody>
-        </table>`;
-        */
+  
 }
 
