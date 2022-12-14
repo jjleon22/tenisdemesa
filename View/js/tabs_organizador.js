3665e099ddfa67be1e6853c135bc5010f3dd88ce
Sergio-a11 <cz.andrs@gmail.com>
2022-12-13 19:56:58 -0500

    nuevo query para partidos y trofeo dinamico

--- View/js/tabs_organizador.js
+++ View/js/tabs_organizador.js
@@ -1,75 +1,122 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    pillActive(0);
+  pillActive(0);
 });
 
 let pestanaActiva = 0;
 
 function pillActive(activo) {
-    let tabsId = ["tabPartidos", "tabParticipantes", "tabHoteles", "tabSalas", "tabCiudad", "tabHistorico"];
-    let tabsName = ["Partidos", "Participantes", "Hoteles", "Salas", "Ciudades", "Historico Campeonatos"];
-    document.getElementById(tabsId[pestanaActiva]).innerHTML =
-        `<li class="nav-item" id="${tabsId[pestanaActiva]}">
+  let tabsId = [
+    "tabPartidos",
+    "tabParticipantes",
+    "tabHoteles",
+    "tabSalas",
+    "tabCiudad",
+    "tabHistorico",
+  ];
+  let tabsName = [
+    "Partidos",
+    "Participantes",
+    "Hoteles",
+    "Salas",
+    "Ciudades",
+    "Historico Campeonatos",
+  ];
+  document.getElementById(
+    tabsId[pestanaActiva]
+  ).innerHTML = `<li class="nav-item" id="${tabsId[pestanaActiva]}">
     <button class="nav-link" onclick="pillActive(${pestanaActiva})">${tabsName[pestanaActiva]}</button>
     </li>
     `;
-    document.getElementById(tabsId[activo]).innerHTML =
-        `<li class="nav-item" id="${tabsId[activo]}">
+  document.getElementById(
+    tabsId[activo]
+  ).innerHTML = `<li class="nav-item" id="${tabsId[activo]}">
     <button class="nav-link active" onclick="pillActive(${activo})">${tabsName[activo]}</button>
     </li>
     `;
-    pestanaActiva = activo;
-    cargarTabla(tabsId[activo]);
+  pestanaActiva = activo;
+  cargarTabla(tabsId[activo]);
 }
 
-
-
 async function cargarTabla(activo) {
+  const elegirTabla = {
+    tabPartidos: "Partido/getPartidos",
+    tabParticipantes: "Participante/getParticipantes",
+    tabSalas: "Sala/getSalas",
+    tabCiudad: "Ciudad/getCiudades",
+    tabHistorico: "Campeonato/getCampeonatos",
+  };
 
-  const elegirTabla = {"tabPartidos": "Partido/getPartidos",
-  "tabParticipantes":"Participante/getParticipantes",
-  "tabSalas":"Sala/getSalas",
-  "tabCiudad":"Ciudad/getCiudades",
-  "tabHistorico":"Campeonato/getCampeonatos"
-};
-
-  const request = await fetch(`http://localhost/tenisdemesa/${elegirTabla[activo]}`, {
-    method: 'GET',
+  try {
+    const request = await fetch(
+      `http://localhost/tenisdemesa/${elegirTabla[activo]}`,
+      {
+        method: "GET",
         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        }
-  });
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-  if (request.ok )
-  {
     const data = await request.json();
     const encabezados = Object.keys(data[0]);
     let dataTable = "";
     let columnsTable = "";
-    encabezados.forEach(i => {
-      columnsTable += `<th scope="col">${i}</th>`;
+    encabezados.forEach((i) => {
+      if(i!="numero_asociado_ganador")
+        columnsTable += `<th scope="col">${i}</th>`;
     });
     columnsTable += `<th scope="col">Editar</th><th scope="col">Eliminar</th>`;
-    data.forEach(i => {
+    data.forEach((i) => {
       //console.log("i",i);
       let contHead = 0;
       let valores = "";
-      
-      encabezados.forEach((j)=>{
+
+      encabezados.forEach((j) => {
         //console.log("j",j)
-        valores += `<td>${i[encabezados[contHead++]]}</td>`;
-      })
-      valores += `<td><button class="btn btn-danger" type="button">
-      <a href="index.html" style="text-decoration: none; color:white"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+        if(encabezados.includes("numero_asociado_ganador"))
+        {
+          if(i["Jugador 1"].includes(i["numero_asociado_ganador"]))
+          {
+            if(j=="Jugador 1")
+            {
+              valores += `<td>${i[encabezados[contHead++]]}&nbsp; 
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trophy-fill" viewBox="0 0 16 16">
+              <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"/>
+              </svg></td>`;
+            }
+          }
+          else{
+            if(j=="Jugador 2")
+            {
+              valores += `<td>${i[encabezados[contHead++]]}&nbsp;
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trophy-fill" viewBox="0 0 16 16">
+              <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"/>
+              </svg></td>`;
+            }
+          }
+        }
+        if(j=="Jugador 2")
+          contHead++;
+          
+        if(contHead<encabezados.length)
+          valores += `<td>${i[encabezados[contHead++]]}</td>`;
+          
+      });
+      //suponiendo que el id simpre va a llegar de primeras
+      let id = i[encabezados[0]];
+      
+      valores += `<td><button class="btn btn-danger" type="button" onclick="cargarUno(${activo},${id})" data-bs-toggle="modal" data-bs-target="#modal${activo}">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
       <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
       <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-      </svg></a>
+      </svg>
       </button></td><td><button class="btn btn-danger" type="button">
-      <a href="index.html" style="text-decoration: none; color:white"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
       <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
       <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-      </svg></a>
-      </button></td>`
+      </svg>
+      </button></td>`;
       dataTable += `
       <tr>${valores}</tr>
         `;
@@ -85,12 +132,38 @@ async function cargarTabla(activo) {
           ${dataTable}
           </tbody>
           </table>`;
-      //console.log("🚀 ~ file: tabs_organizador.js:69 ~ cargarTabla ~ tabla", tabla)
-      
-      document.getElementById("tbOrganizador").innerHTML = tabla;
+    //console.log("🚀 ~ file: tabs_organizador.js:69 ~ cargarTabla ~ tabla", tabla)
+
+    document.getElementById("tbOrganizador").innerHTML = tabla;
+  } catch (error) {
+    console.log(error);
   }
-  else
-    console.log("🚀", "No");
-  
 }
 
+async function cargarUno(activo, id) {
+  const elegirTabla = {
+    tabPartidos: "Partido/getPartido",
+    tabParticipantes: "Participante/getParticipante",
+    tabSalas: "Sala/getSala",
+    tabCiudad: "Ciudad/getCiudad",
+    tabHistorico: "Campeonato/getCampeonato",
+  };
+  try {
+    const request = await fetch(
+      `http://localhost/tenisdemesa/${elegirTabla[activo]}/${id}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = response.json();
+    console.log(request.url);
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
--- View/organizador.php
+++ View/organizador.php
@@ -5,8 +5,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Ping Pow Bogota</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
 </head>
 
 <body>
@@ -16,8 +15,7 @@
     <nav class="navbar navbar-expand-lg bg-light">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">Ping Pow Bogota</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
-          aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
@@ -29,8 +27,8 @@
               <a class="nav-link" href="#">Nosotros</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link active" href="#">Gestion</a>
-              </li>
+              <a class="nav-link active" href="#">Gestion</a>
+            </li>
           </ul>
           <li style="list-style: none;">Hola Juan</li>
           <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
@@ -42,10 +40,10 @@
     </nav>
     <!--fin navbar-->
 
-    
+
 
     <!--contenido main-->
-    <div class="container text-center mt-3">
+    <div class="container-fluid text-center mt-3">
       <div class="row">
         <div class="col">
           <h1>
@@ -74,13 +72,13 @@
           <button class="nav-link" onclick="pillActive(3)">Salas</button>
         </li>
         <li class="nav-item" id="tabCiudad">
-            <button class="nav-link" onclick="pillActive(4)">Ciudades</button>
-          </li>
+          <button class="nav-link" onclick="pillActive(4)">Ciudades</button>
+        </li>
         <li class="nav-item" id="tabHistorico">
-            <button class="nav-link" onclick="pillActive(5)">Historico Campeonatos</button>
+          <button class="nav-link" onclick="pillActive(5)">Historico Campeonatos</button>
         </li>
       </ul>
-      <div class="container-md mt-5">
+      <div class="container-fluid mt-5">
         <!--contenido administrativo-->
         <table class="table" id="tbOrganizador">
           <thead>
@@ -141,20 +139,70 @@
               <td>Jacob&nbsp;&nbsp;</td>
               <td>Thornton</td>
             </tr>
-            
+
           </tbody>
         </table>
         <div class="d-grid gap-2" id="btnCrear">
           <button class="btn btn-primary" type="button">Crear Registro</button>
         </div>
       </div>
-      
-    </div> <!--fin contenido-->
-  </div>
-  <script src="<?php echo BASE_URL;?>View/js/tabs_organizador.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-  crossorigin="anonymous"></script>
+      <!--finc contendio admon-->
+
+      <!-- Modal -->
+
+      <div class="modal fade modal-xl" id="modaltabPartidos" tabindex="-1" aria-labelledby="modaltabPartidos" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="modaltabPartidos">Modal title</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">@</span>
+                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+              </div>
+
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                <span class="input-group-text" id="basic-addon2">@example.com</span>
+              </div>
+
+              <label for="basic-url" class="form-label">Your vanity URL</label>
+              <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon3">https://example.com/users/</span>
+                <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+              </div>
+
+              <div class="input-group mb-3">
+                <span class="input-group-text">$</span>
+                <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+                <span class="input-group-text">.00</span>
+              </div>
+
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Username" aria-label="Username">
+                <span class="input-group-text">@</span>
+                <input type="text" class="form-control" placeholder="Server" aria-label="Server">
+              </div>
+
+              <div class="input-group">
+                <span class="input-group-text">With textarea</span>
+                <textarea class="form-control" aria-label="With textarea"></textarea>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--fin modal-->
+    </div>
+    <!--fin contenido-->
+    <script src="<?php echo BASE_URL; ?>View/js/tabs_organizador.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>
