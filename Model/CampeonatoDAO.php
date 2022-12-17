6c9c42a7678b1925fc719b328ce4f85bb98f1d20
jjleon22 <jjleons@correo.udistrital.edu.co>
2022-12-17 18:05:40 -0500

    Update CampeonatoDAO.php

--- Model/CampeonatoDAO.php
+++ Model/CampeonatoDAO.php
@@ -8,9 +8,16 @@
 
         public function getCampData()
         {
-            $sql = "SELECT * FROM campeonato;";
-            $data = $this->selectAll($sql);
+            
+            $SQL = "CREATE VIEW vista_historico AS SELECT camp.id_campeonato AS \"Id Campeonato\",  
+            camp.nombre AS \"Título\", 
+            concat(c.nombre, ' - ' ,(SELECT nombre from ciudad WHERE camp.id_ciudad=c.id_ciudad)) AS \"Ciudad\",
+            concat(part.nombre, ' - ' ,(SELECT nombre from participante WHERE camp.numero_asociado_ganador=part.numero_asociado)) AS \"Ganador\"
+            FROM campeonato camp JOIN ciudad c ON camp.id_ciudad = c.id_ciudad
+            JOIN participante part ON camp.numero_asociado_ganador = part.numero_asociado;";
+            $data = $this->selectAll($SQL);
             return $data;
+            
         }
     }
 ?>
