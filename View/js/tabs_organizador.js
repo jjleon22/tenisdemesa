c5b0c5b2fc30c2af4dfac1e17c489bbeb34831ad
jjleon22 <jjleons@correo.udistrital.edu.co>
2022-12-17 20:22:37 -0500

    INDEX + JS ORG

--- View/js/tabs_organizador.js
+++ View/js/tabs_organizador.js
@@ -35,6 +35,9 @@ function pillActive(activo) {
     <button class="nav-link active" onclick="pillActive(${activo})">${tabsName[activo]}</button>
     </li>
     `;
+  document.getElementById("btnCrear").innerHTML = `<div class="d-grid gap-2" id="btnCrear">
+  <button class="btn btn-primary" type="button" onclick="crearRegistro(${activo})">Crear Registro</button>
+</div>`;
   pestanaActiva = activo;
   cargarTabla(tabsId[activo]);
 }
@@ -73,12 +76,36 @@ async function cargarTabla(activo) {
     columnsTable += `<th scope="col">Editar</th><th scope="col">Eliminar</th>`;
     if(encabezados.includes("Medios Disponibles"))
     {
-        groupBy(data,"Número Sala")
-        console.log(groupBy[0])
-        //console.log(salas[0])
+      var resultData = Object.values(groupBy(data,"Número Sala"));
+      resultData.forEach((j)=>{
+        let valores = ""
+        console.log(j);
+        valores += `<td>${j["Número Sala"]}</td>`;
+        valores += `<td>${j["Nombre Hotel"]}</td>`;
+        valores += `<td>${j["Medios Disponibles"]}</td>`;
+        valores += `<td>${j["Capacidad"]}</td>`;
+        let id = j[encabezados[0]];
+      
+      valores += `<td><button class="btn btn-warning" type="button" onclick="cargarUno(${activo},${id})" data-bs-toggle="modal" data-bs-target="#modal${activo}">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+      </svg></button></td>
+      <td><button class="btn btn-danger" type="button" data-bs-toggle="modal" data-bs-target="#modalEliminar">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+      <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+      </svg>
+      </button></td>`;
+      dataTable += `
+      <tr>${valores}</tr>
+        `;
+      })
+      console.log(resultData[0])
     }
+    else{
     data.forEach((i) => {
-      console.log("i",i);
+      //console.log("i",i);
       let contHead = 0;
       let valores = "";
       
@@ -120,8 +147,8 @@ async function cargarTabla(activo) {
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
       <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
       <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-      </svg>
-      </button></td><td><button class="btn btn-danger" type="button">
+      </svg></button></td>
+      <td><button class="btn btn-danger" type="button" data-bs-toggle="modal" data-bs-target="#modalEliminar">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
       <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
       <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -131,6 +158,7 @@ async function cargarTabla(activo) {
       <tr>${valores}</tr>
         `;
     });
+  }
     let tabla = `
       <table class="table" id="tbOrganizador">
         <thead>
@@ -267,4 +295,35 @@ var groupBy = function (miarray, prop) {
       groups[val]["Capacidad"] = item["Capacidad"];
       return groups;
   }, {});
+}
+
+function crearRegistro(activo)
+{
+  switch(activo)
+  {
+    case 0:
+    {
+      break;
+    }
+    case 1:
+    {
+      break;
+    }
+    case 2:
+    {
+      break;
+    }
+    case 3:
+    {
+      break;
+    }
+    case 4:
+    {
+      break;
+    }
+    case 5:
+    {
+      break;
+    }
+  }
 }
